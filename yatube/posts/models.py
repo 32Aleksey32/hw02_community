8684fd2da1ce58545384edae7ae35283915f8408
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-04-12 12:17:36 +0300

    2 попытка

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -23,7 +23,7 @@ class Post(models.Model):
     )
     group = models.ForeignKey(
         Group,
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         null=True, blank=True,
-        related_name='group'
+        related_name='posts'
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
+LIMIT = 10
+
 
 # Главная страница
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:LIMIT]
     # В словаре context отправляем информацию в шаблон
     context = {'posts': posts}
     return render(request, 'posts/index.html', context)
@@ -13,7 +15,7 @@ def index(request):
 # Страница с постами
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all().order_by('-pub_date')[:LIMIT]
     context = {
         'group': group,
         'posts': posts,
